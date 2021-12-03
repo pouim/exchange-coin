78f0005cc3d55a5a4e279bc530834c624c35c824
Pouyan Ahmadpour <ahmadpour.pouyan@gmail.com>
2021-12-03 07:48:18 +0330

    fix: (tailwind.config.js)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const colors = require('tailwindcss/colors');
 
 module.exports = {
@@ -5,7 +6,7 @@ module.exports = {
         purgeLayersByDefault: true,
         applyComplexClasses: true,
     },
-    purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class', // 'media' or 'class'
     theme: {
         extend: {
